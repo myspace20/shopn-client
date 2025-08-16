108140941cf06c637be04415ccf2b590997b7b78
rogersatsi <rogersatsi98@gmail.com>
2025-08-16 22:16:05 +0000

    feat: add get transactions api implementation

---
+++ src/features/transaction/api/get-transaction.ts
@@ -0,0 +1,36 @@
+import { api } from "@/lib/api-client"
+import { QueryConfig } from "@/lib/react-query"
+import { Transaction } from "@/types/api"
+import { queryOptions, useQuery } from "@tanstack/react-query"
+
+
+
+
+
+export const getTransactions = (page = 1):Promise<Transaction []> =>{
+    return api.get("/transactions")
+}
+
+
+export const getTransactionsQueryOptions = ({page}:{page?:number}) =>{
+    return queryOptions({
+        queryKey: page ? ['products', {page}] : ['products'],
+        queryFn: () => getTransactions(page)
+    })
+}
+
+
+type UseTransactionOptions = {
+    page?:number,
+    queryConfig?: QueryConfig<typeof getTransactions>
+}
+
+export const useTransactions = ({page, queryConfig}: UseTransactionOptions) =>{
+    return useQuery({
+        ...getTransactionsQueryOptions({page}),
+        ...queryConfig
+    })
+}
+
+
+
