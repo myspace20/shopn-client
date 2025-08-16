302cd63706b58bd0dbf24eb4f22feb15cc6c61c0
rogersatsi <rogersatsi98@gmail.com>
2025-08-16 10:27:03 +0000

    feat: add create product api

---
+++ src/features/product/api/create-product.ts
@@ -0,0 +1,47 @@
+import { api } from "@/lib/api-client";
+import { MutationConfig } from "@/lib/react-query";
+import { Product } from "@/types/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import z from "zod"
+
+
+export const createProductInputSchema = z.object({
+    name: z.string().min(1, "Product name is required"),
+    description: z.string().min(1, "Product description is required"),
+    price: z.number().min(1, "Product price is required"),
+    quantity: z.number().min(1, "Product quantity is required")
+});
+
+
+export type createProductInput = z.infer<typeof createProductInputSchema>;
+
+
+export const createProduct = ({ data }: {
+    data: createProductInput
+}): Promise<Product> => {
+    return api.post("/products", data)
+}
+
+export type UseCreateProductOptions = {
+    mutationConfig?: MutationConfig<typeof createProduct>
+}
+
+
+export const useCreateProduct = ({ mutationConfig }: UseCreateProductOptions = {}) => {
+    const queryClient = useQueryClient()
+
+    const { onSuccess, ...restConfig } = mutationConfig || {};
+
+    return useMutation({
+        onSuccess: (...args) => {
+            queryClient.invalidateQueries({
+
+            });
+            onSuccess?.(...args);
+        },
+        ...restConfig,
+        mutationFn: createProduct
+    })
+}
+
+
