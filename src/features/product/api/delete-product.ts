2cbc17e791736f3e43bd58ee588590a9dfee5c8c
rogersatsi <rogersatsi98@gmail.com>
2025-08-16 12:12:42 +0000

    feat: add delete product api implementation

---
+++ src/features/product/api/delete-product.ts
@@ -0,0 +1,34 @@
+import { api } from "@/lib/api-client"
+import { MutationConfig } from "@/lib/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { getProductsQueryOptions } from "./get-product"
+
+
+
+export const deleteProduct = ({id}: {id: string}) =>{
+    return api.delete(`/products/${id}`)
+}
+
+
+type UseDeleteProductOptions = {
+    mutationConfig?: MutationConfig<typeof deleteProduct>
+}
+
+
+export const UseDeleteProduct = ({mutationConfig}: UseDeleteProductOptions = {}) =>{
+
+    const {onSuccess, ...restConfig} = mutationConfig || {}
+
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        onSuccess: (...args)=>{
+            queryClient.invalidateQueries({
+                queryKey: getProductsQueryOptions().queryKey
+            }),
+            onSuccess?.(...args);
+        },
+        ...restConfig,
+        mutationFn: deleteProduct
+    })
+}
