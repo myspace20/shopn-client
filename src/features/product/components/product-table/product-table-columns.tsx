c11e398c4969ee10ec8e2d51bd50dd0f45f468db
rogersatsi <rogersatsi98@gmail.com>
2025-08-16 20:02:36 +0000

    feat: add product table columns definition

---
+++ src/features/product/components/product-table/product-table-columns.tsx
@@ -0,0 +1,53 @@
+import { Checkbox } from "@/components/ui/checkbox"
+import { Product } from "@/types/api"
+import { ColumnDef } from "@tanstack/react-table"
+
+
+
+
+export const productColumns: ColumnDef<Product>[] = [
+    {
+        id: 'select',
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && 'indeterminate')
+                }
+                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+                className="translate-y-[2px]"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+                className="translate-y-[2px]"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "name",
+        header: "Name",
+    },
+    {
+        accessorKey: "description",
+        header: "Description",
+    },
+    {
+        accessorKey: "price",
+        header: "Price",
+    },
+    {
+        accessorKey: "quantity",
+        header: "Quantity",
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Created at",
+    }
+]
