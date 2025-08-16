6da7e4306dcb183be44e66f92863f4a279bec713
rogersatsi <rogersatsi98@gmail.com>
2025-08-16 08:58:33 +0000

    feat: configure react query

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = createRoot(document.getElementById('root')!);
+
+const queryClient = new QueryClient()
+
 
 
 root.render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
