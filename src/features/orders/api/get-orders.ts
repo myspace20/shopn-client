4a1d4ef12a4cacdbbb2ff5e8948dc604be65aac7
rogersatsi <rogersatsi98@gmail.com>
2025-08-16 20:40:24 +0000

    feat: update get orders api implementation

--- src/features/orders/api/get-orders.ts
+++ src/features/orders/api/get-orders.ts
@@ -1,7 +1,31 @@
 import { api } from "@/lib/api-client"
+import { QueryConfig } from "@/lib/react-query"
+import { Order } from "@/types/api"
+import { queryOptions, useQuery } from "@tanstack/react-query"
 
 
 
-export const getOrders = () =>{
+export const getOrders = (page = 1): Promise<Order []> =>{
     return api.get("/order")
+}
+
+
+export const getOrdersQueryOptions = ({page}:{page?: number} = {}) =>{
+    return queryOptions({
+        queryKey: page ? ['orders', { page }] : ['orders'],
+        queryFn: ()=> getOrders(page)
+    })
+}
+
+
+type UseOrderOptions = {
+    page?: number,
+    queryConfig?: QueryConfig<typeof getOrdersQueryOptions>
+}
+
+export const useOrders = ({queryConfig, page}: UseOrderOptions) =>{
+    return useQuery({
+        ...getOrdersQueryOptions({page}),
+        ...queryConfig
+    })
 }
