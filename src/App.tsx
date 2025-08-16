5db838dbaf5955e5db5b51c0758164e950a1f653
rogersatsi <rogersatsi98@gmail.com>
2025-08-16 20:12:11 +0000

    feat: update application to test dashboard implementations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { AppSidebar } from './components/app-sidebar'
+import { ContentLayout } from './components/layouts/content-layout'
 import { SidebarHeader, SidebarInset, SidebarProvider, SidebarTrigger } from './components/ui/sidebar'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { ProductList } from './features/product/components/view-product'
+import { UserNav } from './components/user-nav'
 
 
 function App() {
@@ -8,23 +11,18 @@ function App() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset >
+      <SidebarInset>
         <SidebarHeader className='bg-sidebar-primary-foreground' >
           <div className='flex justify-between'>
             <SidebarTrigger className='self-center' variant="secondary" />
             <div className='py-2'>
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
+             <UserNav />
             </div>
           </div>
         </SidebarHeader>
-        <main>
-          <div className='p-10'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nisi facere nulla, consequatur animi maxime iusto non asperiores voluptatum odio atque officiis eius doloremque deleniti est pariatur quo iste itaque.
-          </div>
-        </main>
+        <ContentLayout title='Dashboard'>
+         <ProductList />
+        </ContentLayout>
       </SidebarInset>
     </SidebarProvider>
 
