f5f7c8835df17f965af0f2910ad8d58b55a3a0d1
rogersatsi <rogersatsi98@gmail.com>
2025-08-19 12:14:13 +0000

    feat: add route entry point and outlet

---
+++ src/app/routes/app/root.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router';
+
+
+export const ErrorBoundary = () => {
+  return <div>Something went wrong!</div>;
+};
+
+const AppRoot = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default AppRoot;
