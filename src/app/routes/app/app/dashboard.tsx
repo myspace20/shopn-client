8b90dfbf1501000ba65d8f789a6bbca0d8977aeb
rogersatsi <rogersatsi98@gmail.com>
2025-08-19 12:21:44 +0000

    feat: create dashboard route

---
+++ src/app/routes/app/app/dashboard.tsx
@@ -0,0 +1,34 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { ContentLayout } from "@/components/layouts/content-layout"
+import { SidebarProvider, SidebarInset, SidebarHeader, SidebarTrigger } from "@/components/ui/sidebar"
+import { UserNav } from "@/components/user-nav"
+import { Outlet } from "react-router"
+
+
+
+
+
+export const DashboardRoute = () => {
+
+    return (
+        <>
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <SidebarHeader className='bg-sidebar-primary-foreground' >
+                    <div className='flex justify-between'>
+                        <SidebarTrigger className='self-center' variant="secondary" />
+                        <div className='py-2'>
+                            <UserNav />
+                        </div>
+                    </div>
+                </SidebarHeader>
+                <ContentLayout title="Dashboard">
+                        <Outlet />
+                </ContentLayout>
+            </SidebarInset>
+        </SidebarProvider>
+        </>
+    )
+
+}
