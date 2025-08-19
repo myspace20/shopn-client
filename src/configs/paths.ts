cec14594be07a21d013d783b597646f8ec0f71b2
rogersatsi <rogersatsi98@gmail.com>
2025-08-19 12:20:49 +0000

    feat: add dashboard enpoint

---
+++ src/configs/paths.ts
@@ -0,0 +1,14 @@
+
+
+export const paths = {
+    app: {
+        root: {
+            path: '/app',
+            getHref: () => '/app',
+        },
+        dashboard: {
+            path: '/app/dashboard',
+            getHref: () => '/app/dashboard',
+        }
+    },
+}
