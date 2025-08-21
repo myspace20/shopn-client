700a05a74f0fcc25a3fce47321c791fc87e629dc
rogersatsi <rogersatsi98@gmail.com>
2025-08-21 23:32:32 +0000

    feat: add auth enpoints and paths

--- src/configs/paths.ts
+++ src/configs/paths.ts
@@ -11,4 +11,16 @@ export const paths = {
             getHref: () => '/app/dashboard',
         }
     },
+    auth: {
+        register: {
+            path: '/auth/register',
+            getHref: (redirectTo?: string | null | undefined) =>
+                `/auth/register${redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''}`,
+        },
+        login: {
+            path: '/auth/login',
+            getHref: (redirectTo?: string | null | undefined) =>
+                `/auth/login${redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''}`,
+        },
+    },
 }
