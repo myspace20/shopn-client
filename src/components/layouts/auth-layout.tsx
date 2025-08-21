312f154f691584ef8485cbc74a3dfeab4c0468bb
rogersatsi <rogersatsi98@gmail.com>
2025-08-21 23:45:48 +0000

    feat: add auth layout

---
+++ src/components/layouts/auth-layout.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { useEffect } from 'react';
+import { Link, useNavigate, useSearchParams } from 'react-router';
+
+type LayoutProps = {
+  children: React.ReactNode;
+  title: string;
+};
+
+export const AuthLayout = ({ children, title }: LayoutProps) => {
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get('redirectTo');
+
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (user.data) {
+  //     navigate(redirectTo ? redirectTo : paths.app.dashboard.getHref(), {
+  //       replace: true,
+  //     });
+  //   }
+  // }, [user.data, navigate, redirectTo]);
+
+  return (
+    <>
+      {/* <Head title={title} /> */}
+      <div className="flex min-h-screen flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-3 text-center text-3xl font-extrabold text-gray-900">
+            {title}
+          </h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
