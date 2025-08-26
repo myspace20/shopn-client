f2b1a4fd297cf3fa6765a50833a7842ce7172d7b
rogersatsi <rogersatsi98@gmail.com>
2025-08-26 05:56:22 +0000

    feat: add toaster to app root

--- src/app/routes/app/root.tsx
+++ src/app/routes/app/root.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from '@/components/ui/sonner';
 import { Outlet } from 'react-router';
 
 
@@ -9,6 +10,7 @@ const AppRoot = () => {
   return (
     <>
       <Outlet />
+      <Toaster />
     </>
   );
 };
